921db0807398b69e0f415f246cf2a8b6d6d8939d
mariana-cunha <mari.marques26@gmail.com>
2022-10-18 23:53:55 -0300

    ele salva no localstorage e imprime na tela os dados do form

--- script.js
+++ script.js
@@ -25,9 +25,49 @@ function clicar(event) {
 
 botaoFechar.addEventListener("click", popup);
 
+// modal cria card de cadastro
+
+const formModal = document.getElementById("cadastroModal");
+const enviarModal = document.getElementById("enviarModal");
+const lista = document.getElementById("lista");
 
 const cadastroOportunidade = () => {
-    const formulario = document.getElementById("cadastro");
+    formModal.addEventListener("submit", (event) => {
+        event.preventDefault();
 
-    return console.log(formulario.value);
+        const inputNomeModal = document.getElementById("nomeModal").value;
+        const inputFunilModal = document.getElementById("funilModal").value;
+        const inputCampanhaModal = document.getElementById("campanhaModal").value;
+        const inputEmailModal = document.getElementById("emailModal").value;
+
+        localStorage.setItem("nome", inputNomeModal);
+        localStorage.setItem("funil", inputFunilModal);
+        localStorage.setItem("campanha", inputCampanhaModal);
+        localStorage.setItem("email", inputEmailModal);
+
+        modal.classList.toggle("modalAberto");
+
+        // let dadosLocais = JSON.stringify(localStorage);
+        // console.log(dadosLocais)
+    
+        const getDados = JSON.stringify(localStorage);
+        console.log("getDados stringy", getDados)
+
+        // getDados.forEach((item) => {
+        //     criarLista(item);
+        // });
+
+        
+    })
+
+    const criarLista = (text) => {
+        const itemLista = document.createElement("li");
+        itemLista.className = "itens";
+
+        itemLista.innerHTML = text;
+        lista.appendChild(itemLista);
+    }
+    
+
+    
 }
